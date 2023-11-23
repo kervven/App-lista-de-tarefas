ec8f2b97cf81de0c16fe63598429e880b4182f19
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 17:26:04 -0300

    criação da classe Tarefa

---
+++ tarefa.model.php
@@ -0,0 +1,27 @@
+<?php
+
+    class Tarefa{
+        private $id;
+        private $id_status;
+        private $tarefa;
+        private $data_cadastro;
+
+
+        public function __construct($id, $id_status, $tarefa, $data_cadastro)
+        {
+            $this->id = $id;
+            $this->id_status = $id_status;
+            $this->tarefa = $tarefa;
+            $this->data_cadastro = $data_cadastro;
+        }
+
+        public function __get($atributo)
+        {
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor)
+        {
+            $this->$atributo = $valor;
+        }
+    }
