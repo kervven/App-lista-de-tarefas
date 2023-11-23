e756b109f9165426fe7b23642c2befb6cc685f32
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 18:18:13 -0300

    classe do controlador da Tarefa

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -36,10 +36,10 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form>
+								<form method="post" action="tarefa_controller.php">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
-										<input type="text" class="form-control" placeholder="Exemplo: Lavar o carro">
+										<input type="text" class="form-control" name="tarefa" placeholder="Exemplo: Lavar o carro">
 									</div>
 
 									<button class="btn btn-success">Cadastrar</button>
