16183770e4297a17924510cbe8c66939bbe24411
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 17:34:52 -0300

    criado a classe de serviço da Tarefa

---
+++ tarefa.service.php
@@ -0,0 +1,19 @@
+<?php
+
+    class TarefaService {
+        public function inserir() { //create
+
+        }
+
+        public function recuperar() { //read
+
+        }
+
+        public function atualizar() { //update
+
+        }
+
+        public function remover() { //delete
+
+        }
+    }
