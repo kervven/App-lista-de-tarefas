50ba239c46b000e2d691f9af2aca813eb3e409fd
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-26 02:00:28 -0300

    logica de exibir somente atividades pendentes dinamicamente na pagina principal

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+	$acao = 'recuperar';
+	require 'tarefa_controller.php';
+	
+
+
+?>
+
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -7,6 +16,67 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+		<script>
+			function editar(id, txt_tarefa) {
+
+				//criar um form de edição
+				let form = document.createElement('form')
+				form.action = 'tarefa_controller.php?acao=atualizar'
+				form.method = 'post'
+				form.className = 'row'
+
+				//criar um input para entrada do texto
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+				inputTarefa.value = txt_tarefa
+
+				//criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+				//criar um button para envio de form
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				//incluir inputTarefa no form
+				form.appendChild(inputTarefa)
+
+				//incluir inputId no form
+				form.appendChild(inputId)
+
+				//incluir o button no form
+				form.appendChild(button)
+
+				//teste
+				//console.log(form)
+				
+				//alert(txt_tarefa)
+
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				//limpar o texto da tarefa para a inclusão do form
+				tarefa.innerHTML = ''
+
+				//incluir form na pagina
+				tarefa.insertBefore(form, tarefa[0])
+			}
+
+			function remover(id) {
+				location.href = 'todas_tarefas.php?acao=remover&id='+id;
+			}
+
+			function marcarRealizada(id) {
+				location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
+			}
+		</script>
 	</head>
 
 	<body>
@@ -36,23 +106,22 @@
 								<h4>Tarefas pendentes</h4>
 								<hr />
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
+									<?php if ($tarefa->status == 'pendente') { ?>
+									<div class="row mb-3 d-flex align-items-center tarefa">
+										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
+										</div>
+										<div class="col-sm-3 mt-2 d-flex justify-content-between">
+											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+											<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+											
+										</div>
 									</div>
-								</div>
+									<?php } ?>
+								<?php } ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
