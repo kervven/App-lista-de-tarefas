516f0c06fefaffc47eec84d755978a163b93b283
marcosilva2336 <joao1245marcos@outlook.com>
2023-12-15 20:39:47 -0300

    o cara tá pegando fogo mais um teste

--- test/funcoes/TarefaServiceTest.php
+++ test/funcoes/TarefaServiceTest.php
@@ -9,22 +9,27 @@ class TarefaServiceTest extends TestCase
 {
     public function testInserir()
     {
-        // Crie uma instância de Tarefa
         $tarefa = new Tarefa();
-        $tarefa->__set('tarefa', 'Tarefa de Teste'); // Fornecer um valor válido
-
-        // Simule uma instância de Conexao
+        $tarefa->__set('tarefa', 'Tarefa de Teste'); 
         $conexao = new Conexao();
-
-        // Crie uma instância do TarefaService
         $tarefaService = new TarefaService($conexao, $tarefa);
-
-        // Execute o método inserir e verifique se retorna null (sem exceções)
         $this->assertNull($tarefaService->inserir());
     }
 
-    // ... Outros métodos de teste
+    public function testAtualizar()
+    {
+        //Ajustar o id e a tarefa conforme necessário para o seu ambiente de teste. Aqui eu quis atualizar o ID 7
+        $tarefa = new Tarefa();
+        $tarefa->__set('id', 7);
+        $tarefa->__set('tarefa', 'Tarefa Atualizada');
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $this->assertTrue($tarefaService->atualizar());
+    }
+
+  
 }
+
 
 
 
