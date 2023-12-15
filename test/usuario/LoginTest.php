efd062c2dc0fbb0b03467498e52c30f7ecff540f
marcosilva2336 <joao1245marcos@outlook.com>
2023-12-15 11:01:13 -0300

    teste de login

---
+++ test/usuario/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../src/login.php';
+
+class LoginUsuarioTest extends TestCase
+{
+    public function testAutenticacaoCamposVazios()
+    {
+        $loginUsuario = new LoginUsuario();
+        $resultado = $loginUsuario->autenticarUsuario('', '');
+        $this->assertEquals("Por favor, preencha todos os campos.", $resultado);
+    }
+
+    public function testAutenticacaoSucesso()
+    {
+        $loginUsuario = new LoginUsuario();
+        $resultado = $loginUsuario->autenticarUsuario('UsuarioExistente', 'Senha456');
+        $this->assertEquals("Login realizado com sucesso!", $resultado);
+    }
+
+    public function testAutenticacaoFalha()
+    {
+        $loginUsuario = new LoginUsuario();
+        $resultado = $loginUsuario->autenticarUsuario('UsuarioInexistente', 'SenhaIncorreta');
+        $this->assertEquals("Login falhou. Verifique suas credenciais.", $resultado);
+    }
+}
