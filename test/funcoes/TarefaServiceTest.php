9bc5e3ed2b6553ac9f88ac8ebf024eb10934f2c4
marcosilva2336 <joao1245marcos@outlook.com>
2023-12-15 21:15:30 -0300

    finalizado os testes

--- test/funcoes/TarefaServiceTest.php
+++ test/funcoes/TarefaServiceTest.php
@@ -18,9 +18,9 @@ class TarefaServiceTest extends TestCase
 
     public function testAtualizar()
     {
-        //Ajustar o id e a tarefa conforme necessário para o seu ambiente de teste. Aqui eu quis atualizar o ID 7.
+        //Ajustar o id e a tarefa conforme necessário para o seu ambiente de teste. Aqui eu quis atualizar o ID 18.
         $tarefa = new Tarefa();
-        $tarefa->__set('id', 7);
+        $tarefa->__set('id', 18);
         $tarefa->__set('tarefa', 'Tarefa Atualizada');
         $conexao = new Conexao();
         $tarefaService = new TarefaService($conexao, $tarefa);
@@ -31,12 +31,26 @@ class TarefaServiceTest extends TestCase
     {
       //Ajustar o id e a tarefa conforme necessário para o seu ambiente de teste.
         $tarefa = new Tarefa();
-        $tarefa->__set('id', 7);
+        $tarefa->__set('id', 20);
         $tarefa->__set('id_status', 2); //  2 é o ID para o status "Realizada"
         $conexao = new Conexao();
         $tarefaService = new TarefaService($conexao, $tarefa);
         $this->assertTrue($tarefaService->marcarRealizada());
     }
+
+    public function testRemover()
+    {   
+
+        $tarefa = new Tarefa();
+        $tarefa->__set('id', 21);
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $retorno = $tarefaService->remover();
+
+        $this->assertNull($retorno);
+    }
+
 
 }
 
